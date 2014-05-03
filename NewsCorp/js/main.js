eff817d2afd6c9519a61fb69dcf1358328beb49f
Taprolano <mgiambenedetti@yahoo.com>
2014-05-03 16:57:52 +1000

    Added json example file, imported in angular, should work

--- NewsCorp/js/main.js
+++ NewsCorp/js/main.js
@@ -1,6 +1,19 @@
 /**
  * Created by mgiam_000 on 5/3/14.
  */
-var newsCorpAppModule1 = angular.module('newsCorpAppModule1', []);
+var $pathToData = 'json/example.json'; //EXAMPLE JSON FILE. TO BE CHANGED WITH ACTUAL JSON FEED
+var newsCorpAppModule1 = angular.module('newsCorpApp', []);
 
-newsCorpAppModule1.controller=function(){};
+newsCorpAppModule1.controller('module1', ['$scope', '$http', function(scope, http){
+    scope.data = {
+        articles : {}
+    };
+    scope.getData = function(){
+        //var articles = scope.data.articles;
+        http
+            .get($pathToData)
+            .success(function(data){
+                scope.data.articles = data
+            })
+    }
+}]);
